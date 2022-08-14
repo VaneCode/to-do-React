05b57fae57d21156537570adf48c1c179dfaf238
vanessaBonito <vanebonito@hotmail.com>
2022-08-13 23:02:10 -0500

    Add h1 inline css styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,19 @@ import React from "react"
 const Header = () => {
   return (
     <header>
-      <h1>todos</h1>
+      <h1
+        style={{
+          fontSize: "6rem",
+          fontWeight: "600",
+          marginBottom: "2rem",
+          lineHeight: "1em",
+          color: "#ececec",
+          textTransform: "lowercase",
+          textAlign: "center",
+        }}
+      >
+        todos
+      </h1>
     </header>
   )
 }
