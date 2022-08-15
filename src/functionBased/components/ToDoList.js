2a37ebc2f8ad19785a352909e8f384aed8b0940c
vanessaBonito <vanebonito@hotmail.com>
2022-08-15 17:47:03 -0500

    Change ToDoList to a functional component

--- src/functionBased/components/ToDoList.js
+++ src/functionBased/components/ToDoList.js
@@ -1,25 +1,23 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/prop-types */
-import React from 'react';
-import ToDoItem from './ToDoItem';
+import React from "react";
+import ToDoItem from "./ToDoItem";
 
-class ToDoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-          <ToDoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-            setUpdate={this.props.setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const ToDoList = (props) => {
+  return (
+    <ul>
+      {props.todos.map((todo) => (
+        <ToDoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+          setUpdate={props.setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default ToDoList;
