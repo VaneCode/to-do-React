5e924f4078f81775bc8059ec09e5a5bdd2f2982b
vanessaBonito <vanebonito@hotmail.com>
2022-08-14 21:42:53 -0500

    Change css to scss

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,19 +1,20 @@
 import React from "react";
-
+import styles from "./ToDoItem.module.scss"
 class ToDoItem extends React.Component {
   render() {
     return (
-      <li>
-        <input
-          type="checkbox"
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-          Delete
-        </button>
-        {this.props.todo.title}
-      </li>
+      <li className={styles.item}>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        checked={this.props.todo.completed}
+        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+      />
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+      </button>
+      {this.props.todo.title}
+    </li>
     );
   }
 }
