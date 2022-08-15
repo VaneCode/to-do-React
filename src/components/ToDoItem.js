a1805f510fcaca3cd5d6e43f1f155a4114767d77
vanessaBonito <vanebonito@hotmail.com>
2022-08-14 22:16:34 -0500

    Add completed task style

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,20 +1,28 @@
 import React from "react";
-import styles from "./ToDoItem.module.scss"
+import styles from "./ToDoItem.module.css";
 class ToDoItem extends React.Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
     return (
       <li className={styles.item}>
-      <input
-        type="checkbox"
-        className={styles.checkbox}
-        checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-      />
-      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-        Delete
-      </button>
-      {this.props.todo.title}
-    </li>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+        </span>
+      </li>
     );
   }
 }
