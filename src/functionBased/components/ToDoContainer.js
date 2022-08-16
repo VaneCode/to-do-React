3f7c525d75ff276fe32ba78919be1871381657ca
vanessaBonito <vanebonito@hotmail.com>
2022-08-16 12:40:08 -0500

    Add styles to about page

--- src/functionBased/components/ToDoContainer.js
+++ src/functionBased/components/ToDoContainer.js
@@ -1,25 +1,28 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint no-param-reassign: "error" */
-import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import Header from './Header';
-import InputToDo from './InputToDo';
-import ToDoList from './ToDoList';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import SinglePage from "../pages/SinglePage";
+import Header from "./Header";
+import InputToDo from "./InputToDo";
+import ToDoList from "./ToDoList";
+import SinglePage from "../pages/SinglePage";
 
 const ToDoContainer = () => {
   const getInitialTodos = () => {
     // getting stored items
-    const temp = localStorage.getItem('todos');
+    const temp = localStorage.getItem("todos");
     const savedTodos = JSON.parse(temp);
     return savedTodos || [];
   };
   const [todos, setTodos] = useState(getInitialTodos);
 
   useEffect(() => {
-    console.log('test run');
+    console.log("test run");
 
     // getting stored items
-    const temp = localStorage.getItem('todos');
+    const temp = localStorage.getItem("todos");
     const loadedTodos = JSON.parse(temp);
 
     if (loadedTodos) {
@@ -28,27 +31,27 @@ const ToDoContainer = () => {
   }, [setTodos]);
 
   const handleChange = (id) => {
-    setTodos((prevState) => prevState.map((todo) => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      }
-      return todo;
-    }));
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
   };
 
   useEffect(() => {
     // storing todos items
     const temp = JSON.stringify(todos);
-    localStorage.setItem('todos', temp);
+    localStorage.setItem("todos", temp);
   }, [todos]);
 
   const delTodo = (id) => {
-    setTodos([
-      ...todos.filter((todo) => todo.id !== id),
-    ]);
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
   };
 
   const addTodoItem = (title) => {
@@ -67,23 +70,31 @@ const ToDoContainer = () => {
           todo.title = updatedTitle;
         }
         return todo;
-      }),
+      })
     );
   };
 
   return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputToDo addTodoProps={addTodoItem} />
-        <ToDoList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
-      </div>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputToDo addTodoProps={addTodoItem} />
+              <ToDoList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>
+        }
+      />
+      <Route path="/SinglePage" element={<SinglePage />} />
+    </Routes>
   );
 };
 export default ToDoContainer;
--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const SinglePage = () => {
   const aboutData = [
     {
-      slug: "about-app",
-      title: "About the App",
+      slug: 'about-app',
+      title: 'About the App',
       description:
-        "In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.",
+        'In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.',
     },
     {
-      slug: "about-author",
-      title: "Vanessa Bonito",
+      slug: 'about-author',
+      title: 'Vanessa Bonito',
       description:
-        "Remote full-stack software developer, passionate about learning new technologies.",
+        'Remote full-stack software developer, passionate about learning new technologies.',
     },
   ];
 
